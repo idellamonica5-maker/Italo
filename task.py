954ae4d3b84a77168c69762dd2c22aeb8d9316e7
Italo <i.dellamonica5@studenti.unisa.it>
2026-05-25 16:06:27 +0200

    Aggiunta del calcolo del polinomio di Taylor

--- task.py
+++ task.py
@@ -28,18 +28,23 @@ def calcola_limite(espressione: str, variabile: str, punto: str) -> sympy.Expr:
     pass
 
 def calcola_polinomio_taylor(espressione: str, variabile: str, punto: float, ordine: int) -> sympy.Expr:
-    """Sub-task 4: Calcolare una Serie di Taylor."""
+
     pass
 
 def risolvi_sistema_lineare(eq1: str, eq2: str, var1: str, var2: str) -> Dict[sympy.Symbol, sympy.Expr]:
-    """Sub-task 5: Risolvere un Sistema Lineare."""
+    eq1=sympy.sympify(eq1)
+    eq2=sympy.sympify(eq2)
+    var1=sympy.sympify(var1)
+    var2=sympy.sympify(var2)
+    risolvi_sistema_lineare = sympy.solve([eq1, eq2], (var1, var2))
+    return risolvi_sistema_lineare
     pass
 
 def main():
     print("Sub-task 1:", calcola_derivata("x**3 +2*x", "x"))
     print("Sub-task 2:", calcola_integrale_definito("x", "x", 0, 1))
     print("Sub-task 3:", calcola_limite("(x**2 - 1)/(x-1)", "x", "1"))
-    print("Sub-task 4:", calcola_polinomio_taylor("exp(x)", "x", 0.0, 4))
+    print("Sub-task 4:", calcola_polinomio_taylor("cos(x)", "x", 0.0, 3))
     print("Sub-task 5:", risolvi_sistema_lineare("x + y - 3", "x - y - 1", "x", "y"))
 
 if __name__ == "__main__":
